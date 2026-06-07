f58c60e0443dc409909768a2cc5979b056e9536a
Joseph Ng'ang'a <167590487+Joseous@users.noreply.github.com>
2026-06-08 01:34:55 +0300

    Update 02_dimensions_exploration.sql

--- scripts/02_dimensions_exploration.sql
+++ scripts/02_dimensions_exploration.sql
@@ -1,1 +1,21 @@
+--===========================================================
+--         Dimensions Exploration
+-- Identify the unique values or categories in each dimension
+-- Recognizing how data might be grouped or segmented, which is useful for later analysis
+-- example: DISTINCT country, category, product ....
+--===========================================================
 
+-- Explore all countries our customers come from
+
+SELECT DISTINCT
+	country
+FROM gold.dim_customers
+
+-- Explore All product Categories, The major division
+
+SELECT DISTINCT 
+	category,
+	subcategory,
+	product_name
+FROM gold.dim_products
+ORDER BY 1,2,3
