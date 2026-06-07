efe5b5ccaa36f55c66f5bc98698ddec4b0462712
Joseph Ng'ang'a <167590487+Joseous@users.noreply.github.com>
2026-06-08 01:37:08 +0300

    Update 03_date_range_exploration.sql

--- scripts/03_date_range_exploration.sql
+++ scripts/03_date_range_exploration.sql
@@ -1,1 +1,25 @@
+--==================================================
+--       Date Exploration
+-- identify the eariest and latest dates(bounderies)
+-- Understand the scope of data and timespan
+-- MIN/MAX[Date Dimension]
+--===================================================
+
+-- Find the date of the first and last order
+-- How many years of nsales are available
+
+SELECT 
+	MIN(order_date) AS first_order_date,
+	MAX(order_date) AS last_order_date,
+	DATEDIFF(year, MIN(order_date), MAX(order_date)) AS order_range_years 
+FROM gold.fact_sales
+
+-- Find the youngest and oldest customer based on birth_date
+
+SELECT 
+	MIN(birth_date) AS oldest_birthdate,
+	DATEDIFF(year, MIN(birth_date), GETDATE()) AS oldest_age,
+	MAX(birth_date) AS youngest_birthdate,
+	DATEDIFF(year, MAX(birth_date), GETDATE()) AS youngest_age
+FROM gold.dim_customers
 
