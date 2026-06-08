d80eb133683ff6479c9b4cba06393464d8035e98
Joseph Ng'ang'a <167590487+Joseous@users.noreply.github.com>
2026-06-08 03:22:27 +0300

    Update 11_part_to_whole_analysis.sql

--- scripts/11_part_to_whole_analysis.sql
+++ scripts/11_part_to_whole_analysis.sql
@@ -1,1 +1,26 @@
+/*====================================================================================================
+--          Part-To-Whole Analysis
 
+-- Analyze how individual aprt is performing compared to the overall
+-- Allowing us to understand which category has the greatest impact on the business.
+-- ([Measure] / Total[Measure]) * 100 by [dimension]
+--=======================================================================================================*/
+
+-- which categories contribute the most to overall sales?
+
+WITH category_sales AS (
+SELECT 
+	category,
+	SUM(sales_amount) AS total_sales
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+GROUP BY category)
+
+SELECT
+	category,
+	total_sales,
+	SUM(total_sales) OVER() AS overall_sales,
+	CONCAT(ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER()) * 100, 2), '%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC
